466f93135efacaca1d7ef2bd3100860430de5798
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-07-02 21:40:09 -0500

    Handles game continue and running out of words

--- index.js
+++ index.js
@@ -8,36 +8,49 @@
 var Word = require('./Word.js');
 var inquirer = require('inquirer');
 
-possibleWords = ['parallel'];  // array of words to choose from
+var numWins = 0;
+var numLosses = 0;
+const maxGuesses = 6; // starting number of guesses
+var numGuesses = 0;
+var gameSolved = false;
+var guessedLetters = [];
+var possibleWords = ['square', 'rhombus', 'hexagon', 'circle'];  // array of words to choose from
 var currentWord = new Word();
+
+selectWord();
 
 /** Randomly selects a word and initializes it
  * 
  */
 function selectWord () {
     if (possibleWords.length >0) {
-    let idx = Math.floor(Math.random()*possibleWords.length);
-    let selected = possibleWords[idx];
-    currentWord.initialize(selected);
-    possibleWords.splice(idx,1); // removes the word from the array
+        let idx = Math.floor(Math.random()*possibleWords.length);
+        let selected = possibleWords[idx];
+        currentWord.initialize(selected);
+        possibleWords.splice(idx,1); 
+        numGuesses = maxGuesses; // re-set guesses
+        guessedLetters = []; // re-set guessed letters
+        gameSolved = false; // re-set game state
+        playGame();
     } else {
-        console.log ('All my words have been used!');
+        console.log ('\nAll my words have been used!');
     }
 }
 
 
-numGuesses = 10; // starting numbner of guesses
-guessedLetters = [];
 
 function playGame() {
-    if (numGuesses > 0) {
+    if (numGuesses > 0 && !gameSolved) {
+        console.log('----------\n');
         console.log(currentWord.displayWord());
+        console.log('\n');
         console.log(`You have ${numGuesses} guesses remaining.`)
         inquirer
             .prompt ([
                 {
                     type: 'input',
                     name: 'letterGuess',
+                    message: 'Guess a letter',
                     validate: function(x) {
                         if (/^[a-z]{1}$/i.test(x)) {
                             return true
@@ -54,15 +67,17 @@ function playGame() {
                         console.log (`Your guess was ${guessResult}`);
                         break;
                     case 'duplicate':
-                        console.log (`Your guess was ${guessResult}`);
+                        console.log (`Your guess was a ${guessResult}`);
                         break;
                     case 'incorrect':
                         console.log (`Your guess was ${guessResult}`);
                         numGuesses--;
                         break;
                     case 'solved':
+                        numWins++;
                         console.log('You solved the puzzle!');
                         console.log(currentWord.displayWord());
+                        gameSolved = true;
                         break;
                     default:
                         console.log('unhandled result');
@@ -72,13 +87,34 @@ function playGame() {
             });
     } else {
         // you're done!
-        console.log('You lose!  You have no guesses remaining.');
+        if (gameSolved) {
+            console.log('You won!')
+        } else {
+            numLosses++;
+            console.log('You lose!  You have no guesses remaining.');
+        }
+        endGame();
     }    
 }
 
+function endGame () {
+    console.log (`You have ${numWins} wins and ${numLosses} losses.`);
+    inquirer
+    .prompt ([
+        {
+            type: 'confirm',
+            message: 'Do you want to play again?',
+            name: 'continueYN'
+        }
+    ])
+    .then ( (r) => {
+        if (r.continueYN) {
+            selectWord();
+        }
+    });
 
-selectWord();
-playGame();
+}
+
 
 /*
 let testWord = process.argv[2];
